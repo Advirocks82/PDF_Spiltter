64f65e383b435cfe686695538eaf2dd25d4ed61f
Aditya <104798214+Advirocks82@users.noreply.github.com>
2023-02-27 03:30:49 +0530

    Add files via upload

---
+++ snakegame/Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snakegame;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+/**
+ *
+ * @author hp
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        JFrame frame=new JFrame("snake Game");
+        frame.setBounds(10,10,905,700);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        GamePanel panel = new GamePanel();
+        panel.setBackground(Color.DARK_GRAY);
+        frame.add(panel);
+        
+        frame.setVisible(true);
+    }
+}
+    
